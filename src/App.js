1af8fa7d9e61d43fd6033ca558bb3ec5712ee2bf
Gaurav Singh <grvsgh7@gmail.com>
2021-07-07 20:53:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [newTask, setNewTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+  const addTask = () => {
+    setTasks([...tasks, newTask]);
+  }
+  const deleteTask = (index) => {
+    var duplicateArray = [...tasks];
+    duplicateArray.splice(index, 1);
+    setTasks(duplicateArray);
+  }
+  const taskList = tasks.map((task, index) => {
+    return (<div className='row justify-content-center'>
+      <div className='col-md-6 '>
+        <h1>{index + 1}.{task}</h1>
+      </div>
+      <div className='col-md-1'>
+        <button className="btn btn-danger" onClick={() => deleteTask(index)}>DELETE</button>
+      </div>
+    </div>);
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='row justify-content-center'>
+      <h1 className='text-center'>React Todo List</h1>
+      <div className='row justify-content-center'>
+        <div className='col-md-6'>
+          <input type='text' placeholder='taskname' className='form-control' value={newTask} onChange={(e) => setNewTask(e.target.value)} />
+        </div>
+        <div className='col-md-1'>
+          <button className='btn btn-success' onClick={addTask}>Add Task</button>
+        </div>
+      </div>
+
+      {taskList}
+
     </div>
   );
 }
